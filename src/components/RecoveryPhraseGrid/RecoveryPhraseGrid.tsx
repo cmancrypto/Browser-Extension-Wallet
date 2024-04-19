ecf1dbc5f075df3dae9eee209c6f33b66232f41e
Vasyl Stasiuk <vasyl.stasiuk@wetelo.com>
2024-04-19 18:07:29 +0300

    feat: updated RecoveryPhraseGrid functionality to cover different scenarios(read and write mods)

--- src/components/RecoveryPhraseGrid/RecoveryPhraseGrid.tsx
+++ src/components/RecoveryPhraseGrid/RecoveryPhraseGrid.tsx
@@ -1,15 +1,18 @@
 import { ClassValue } from 'clsx';
 import React, { useMemo, useState } from 'react';
 
-import { Copy, EyeOpen } from '@/assets/icons';
+import { Copy, EyeClose, EyeOpen } from '@/assets/icons';
+import { EMPTY_RECOVERY_PHRASE } from '@/constants';
 import { cn } from '@/helpers/utils';
 import { Button, Input } from '@/ui-kit';
 
 type RecoveryPhraseGridProps = {
-  phrase: { id: number; value: string }[];
+  phrase?: { id: number; value: string }[];
   withButtons?: boolean;
   showPhrase?: boolean;
   verifyPhrase?: boolean;
+  readOnly?: boolean;
+  handleResult?: (index: number, value: string) => void;
 };
 
 const HIDDEN_INDEXES = [1, 5, 8];
@@ -19,15 +22,19 @@ export const RecoveryPhraseGrid: React.FC<RecoveryPhraseGridProps> = ({
   withButtons = false,
   showPhrase = true,
   verifyPhrase = false,
+  readOnly = false,
+  handleResult,
 }) => {
   const [isShown, setIsShown] = useState<boolean>(!!showPhrase);
 
-  const joinedPhraseString = useMemo(() => phrase.map(word => word.value).join(' '), [phrase]);
+  const joinedPhraseString = useMemo(() => phrase?.map(word => word.value).join(' '), [phrase]);
 
   const resultPhrase = useMemo(() => {
+    if (!phrase) return EMPTY_RECOVERY_PHRASE;
+
     if (!verifyPhrase) return phrase;
 
-    const result = phrase.map((item, index) =>
+    const result = phrase?.map((item, index) =>
       HIDDEN_INDEXES.includes(index) ? { id: item.id, value: '' } : item,
     );
 
@@ -39,21 +46,32 @@ export const RecoveryPhraseGrid: React.FC<RecoveryPhraseGridProps> = ({
   };
 
   const handleCopyToClipboard = () => {
+    if (!joinedPhraseString) return;
+
     navigator.clipboard.writeText(joinedPhraseString);
+  };
+
+  const onChangeInput = (index: number, value: string) => {
+    if (!handleResult) return;
+
+    handleResult(index, value);
   };
 
   return (
     <div className="w-full">
-      <ul className="grid grid-cols-3 gap-y-2.5 gap-x-2.5 p-2.5 border border-grey rounded-lg">
+      <ul className="grid grid-cols-3 gap-y-3.5 gap-x-2.5 p-2.5 border border-grey rounded-lg">
         {resultPhrase.map((word, index) => (
           <li key={word.id} className="inline-flex items-center max-w-full">
-            <span className="mr-1 text-base/[25px] text-white w-5 text-left">{index + 1}.</span>
+            <span className="mr-1 text-sm/[24px] text-white w-5 text-left">{index + 1}.</span>
             <Input
+              type="password"
+              onChange={e => onChangeInput(index, e.target.value)}
+              readOnly={readOnly || (verifyPhrase && !HIDDEN_INDEXES.includes(index))}
               className={cn(
-                'border border-white text-white rounded-3xl h-[30px] px-3 py-1.5 w-full focus:outline-0 text-center',
+                'border border-white text-white text-sm rounded-3xl h-6 px-2 py-1 pb-[5px] w-full focus:outline-0 text-center',
                 (verifyPhrase && HIDDEN_INDEXES.includes(index) && 'border-blue') as ClassValue,
               )}
-              value={isShown ? word.value : '*****'}
+              defaultValue={isShown ? word.value : '*****'}
             />
           </li>
         ))}
@@ -61,12 +79,12 @@ export const RecoveryPhraseGrid: React.FC<RecoveryPhraseGridProps> = ({
       {withButtons && (
         <div className="flex justify-between px-5 mt-4">
           <Button variant="transparent" size="small" onClick={handleShowPhrase}>
-            <EyeOpen />
-            <span className="ml-2.5 text-lg">Hide seed phrase</span>
+            {isShown ? <EyeClose height={20} /> : <EyeOpen height={20} />}
+            <span className="ml-2.5 text-base">{isShown ? 'Hide' : 'Show'} seed phrase</span>
           </Button>
           <Button variant="transparent" size="small" onClick={handleCopyToClipboard}>
-            <Copy />
-            <span className="ml-2.5 text-lg">Copy to clipboard</span>
+            <Copy width={20} />
+            <span className="ml-2.5 text-base">Copy to clipboard</span>
           </Button>
         </div>
       )}
