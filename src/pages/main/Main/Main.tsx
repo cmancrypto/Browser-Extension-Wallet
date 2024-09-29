c6eb150f2b1495194e779f8486db539f85598335
dblackstone1 <dblackstone@orchestralabs.org>
2024-09-29 16:12:24 -0400

    feat: remove unnecessary select options

--- src/pages/main/Main/Main.tsx
+++ src/pages/main/Main/Main.tsx
@@ -1,8 +1,7 @@
-import { Select, SelectValue } from '@radix-ui/react-select';
 import { NavLink } from 'react-router-dom';
 import { AssetScroller, ReceiveDialog } from '@/components';
 import { ROUTES } from '@/constants';
-import { Button, SelectContent, SelectItem, SelectSeparator, SelectTrigger } from '@/ui-kit';
+import { Button } from '@/ui-kit';
 import { useAtomValue } from 'jotai';
 import { walletStateAtom } from '@/atoms';
 
@@ -10,44 +9,7 @@ export const Main = () => {
   const walletState = useAtomValue(walletStateAtom);
 
   return (
-    <div className="h-full pt-5">
-      <div className="flex gap-x-6 px-4">
-        <div className="text-left">
-          <label className="text-sm text-neutral-1">Account 1</label>
-          <Select defaultValue="1ZChms...HVDzw">
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Account" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="1ZChms...HVDzw">1ZChms...HVDzw</SelectItem>
-              <SelectSeparator />
-              <SelectItem value="1ZChms...HbHe1">1ZChms...HbHe1</SelectItem>
-              <SelectSeparator />
-              <SelectItem value="1ZChms...H24Kvh">1ZChms...H24Kvh</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="text-left">
-          <label className="text-sm text-neutral-1 mb-0.5">Network</label>
-          <Select defaultValue="symphony">
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Network" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="symphony">Symphony</SelectItem>
-              <SelectSeparator />
-              <SelectItem value="ethereum">Ethereum</SelectItem>
-              <SelectSeparator />
-              <NavLink
-                to={ROUTES.APP.ADD_NETWORK}
-                className="flex text-blue font-normal hover:text-blue-dark py-2 px-2.5 focus:outline-0"
-              >
-                Add new network
-              </NavLink>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
+    <div className="h-full">
       <div className="px-4 mt-4">
         <div className="p-4 border rounded-xl border-neutral-4 flex flex-col items-center">
           <div className="text-center mb-7">
