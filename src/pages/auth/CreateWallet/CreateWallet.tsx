adb549cfafb9c96bf22c912142c3152b75f0a3b3
dblackstone1 <dblackstone@orchestralabs.org>
2024-10-19 20:12:58 -0400

    chore: update todos

--- src/pages/auth/CreateWallet/CreateWallet.tsx
+++ src/pages/auth/CreateWallet/CreateWallet.tsx
@@ -97,6 +97,7 @@ export const CreateWallet = () => {
   /* Current TODOs */
   // TODO: provide updates to user as staking, claim, and unstaking information changes (successes, errors, and changing gas fees)
 
+  // TODO: change receiving asset with sending asset until receive asset is changed through saved wallet or directly
   // TODO: provide updates to user as send information updates (errors and changing gas fees)
   // TODO: add mock max amount placeholder to amount sections (send and validator)
   // TODO: add clear and max buttons to amount section
